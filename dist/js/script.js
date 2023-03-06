008c6f56a9cebb583c8e11486f42435057f0bd3c
Andrii9292 <119946292+Andrii9292@users.noreply.github.com>
2023-03-06 20:35:54 +0900

    .

--- dist/js/script.js
+++ dist/js/script.js
@@ -6,3 +6,24 @@ thumbnails.forEach(thumbnail => {
         preview.src = thumbnail.src;
     });
 });
+//--------------------слайдер-------------------//
+$(document).ready(function () {
+    var $slider = $('.unique__card-wrapper');
+    var $counter = $('.counter');
+
+    $slider.on('init reInit afterChange', function (event, slick, currentSlide) {
+        var i = (currentSlide ? currentSlide : 0) + 1;
+        $counter.text(i + '/' + slick.slideCount);
+    });
+
+    $slider.slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        prevArrow: '<img class="slick-prev" src="../images/prev-arrow.svg">',
+        nextArrow: '<img class="slick-next" src="../images/next-arrow.svg">',
+        infinite: true,
+        dots: false
+    });
+});
+
+
